d1de0669771cdaef72f454e94a80a5077a318056
Michael Irwin <mike.irwin1@gmail.com>
2019-01-12 14:43:35 +0000

    entered form data into page

--- js/app.js
+++ js/app.js
@@ -8,5 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleFormSubmit = function (event) {
   event.preventDefault();
   console.dir(event);
-  
+
+  const list = document.querySelector('#read-list');
+  const newListItem = document.createElement('li');
+  const title = document.createElement('h1');
+  const author = document.createElement('h3');
+  const genre = document.createElement('h4');
+
+  title.textContent = `${event.target.title.value}`;
+  author.textContent = `${event.target.author.value}`;
+  genre.textContent = `${event.target.genre.value}`;
+  list.appendChild(newListItem);
+  newListItem.appendChild(title);
+  newListItem.appendChild(author);
+  newListItem.appendChild(genre);
+  event.target.reset();
 };
